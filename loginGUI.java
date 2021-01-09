ab474072524eabc2aafce973ce471673197efc36
Dominic Perera <wperera@mail.valenciacollege.edu>
2021-01-08 23:02:01 -0500

    Create a Frame

--- loginGUI.java
+++ loginGUI.java
@@ -1,9 +1,19 @@
+import javax.swing.*;
+
 public class loginGUI{
 
   public static void main(String[] args) {
 
-    
 
+    //create a frame object
+    JFrame frame = new JFrame();
+    frame.setSize(300, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+
+
+    //create a panel object
+    JPanel panel = new JPanel();
 
   }
 
